6a428a6d9d0833ca40851ba699e0075e79ccba51
mfaine <{'changed': False, 'results': [], 'failed': False}>
2024-04-14 15:43:54 -0500

    Restore correct version of zim init files

--- zsh/.zim/init.zsh
+++ zsh/.zim/init.zsh
@@ -1,43 +1,5 @@
-zimfw() { source /home/mfaine/.zim/zimfw.zsh "${@}" }
-zmodule() { source /home/mfaine/.zim/zimfw.zsh "${@}" }
-fpath=(/dotfiles/zsh/.zim/modules/git/functions /dotfiles/zsh/.zim/modules/utility/functions /dotfiles/zsh/.zim/modules/duration-info/functions /dotfiles/zsh/.zim/modules/git-info/functions /dotfiles/zsh/.zim/modules/zsh-completions/src /dotfiles/zsh/.zim/modules/pvenv/functions /dotfiles/zsh/.zim/modules/utility/functions /dotfiles/zsh/.zim/modules/duration-info/functions /dotfiles/zsh/.zim/modules/git-info/functions /dotfiles/zsh/.zim/modules/zsh-completions/src /dotfiles/zsh/.zim/modules/utility/functions /dotfiles/zsh/.zim/modules/duration-info/functions /dotfiles/zsh/.zim/modules/git-info/functions /dotfiles/zsh/.zim/modules/zsh-completions/src /dotfiles/zsh/.zim/modules/prompt-pwd/functions /dotfiles/zsh/.zim/modules/archive/functions /dotfiles/zsh/.zim/modules/asdf/functions /dotfiles/zsh/.zim/modules/k/functions /dotfiles/zsh/.zim/modules/zim-yq/functions /dotfiles/zsh/.zim/modules/zim-gopass/functions ${fpath})
-autoload -Uz -- git-alias-lookup git-branch-current git-branch-delete-interactive git-branch-remote-tracking git-dir git-ignore-add git-root git-stash-clear-interactive git-stash-recover git-submodule-move git-submodule-remove mkcd mkpw duration-info-precmd duration-info-preexec coalesce git-action git-info pvenv mkcd mkpw duration-info-precmd duration-info-preexec coalesce git-action git-info mkcd mkpw duration-info-precmd duration-info-preexec coalesce git-action git-info prompt-pwd archive lsarchive unarchive asdf k
-source /dotfiles/zsh/.zim/modules/environment/init.zsh
-source /dotfiles/zsh/.zim/modules/environment/init.zsh
-source /dotfiles/zsh/.zim/modules/environment/init.zsh
-source /dotfiles/zsh/.zim/modules/git/init.zsh
-source /dotfiles/zsh/.zim/modules/input/init.zsh
-source /dotfiles/zsh/.zim/modules/input/init.zsh
-source /dotfiles/zsh/.zim/modules/input/init.zsh
-source /dotfiles/zsh/.zim/modules/termtitle/init.zsh
-source /dotfiles/zsh/.zim/modules/termtitle/init.zsh
-source /dotfiles/zsh/.zim/modules/termtitle/init.zsh
-source /dotfiles/zsh/.zim/modules/utility/init.zsh
-source /dotfiles/zsh/.zim/modules/utility/init.zsh
-source /dotfiles/zsh/.zim/modules/utility/init.zsh
-source /dotfiles/zsh/.zim/modules/duration-info/init.zsh
-source /dotfiles/zsh/.zim/modules/duration-info/init.zsh
-source /dotfiles/zsh/.zim/modules/duration-info/init.zsh
-source /dotfiles/zsh/.zim/modules/asciiship/asciiship.zsh-theme
-source /dotfiles/zsh/.zim/modules/asciiship/asciiship.zsh-theme
-source /dotfiles/zsh/.zim/modules/asciiship/asciiship.zsh-theme
-source /dotfiles/zsh/.zim/modules/completion/init.zsh
-source /dotfiles/zsh/.zim/modules/completion/init.zsh
-source /dotfiles/zsh/.zim/modules/completion/init.zsh
-source /dotfiles/zsh/.zim/modules/zsh-syntax-highlighting/zsh-syntax-highlighting.zsh
-source /dotfiles/zsh/.zim/modules/zsh-syntax-highlighting/zsh-syntax-highlighting.zsh
-source /dotfiles/zsh/.zim/modules/zsh-syntax-highlighting/zsh-syntax-highlighting.zsh
-source /dotfiles/zsh/.zim/modules/zsh-history-substring-search/zsh-history-substring-search.zsh
-source /dotfiles/zsh/.zim/modules/zsh-history-substring-search/zsh-history-substring-search.zsh
-source /dotfiles/zsh/.zim/modules/zsh-history-substring-search/zsh-history-substring-search.zsh
-source /dotfiles/zsh/.zim/modules/zsh-autosuggestions/zsh-autosuggestions.zsh
-source /dotfiles/zsh/.zim/modules/zsh-autosuggestions/zsh-autosuggestions.zsh
-source /dotfiles/zsh/.zim/modules/zsh-autosuggestions/zsh-autosuggestions.zsh
-source /dotfiles/zsh/.zim/modules/gitster/gitster.zsh-theme
-source /dotfiles/zsh/.zim/modules/ssh/init.zsh
-source /dotfiles/zsh/.zim/modules/fzf/init.zsh
-source /dotfiles/zsh/.zim/modules/archive/init.zsh
-source /dotfiles/zsh/.zim/modules/asdf/init.zsh
-source /dotfiles/zsh/.zim/modules/k/init.zsh
-source /dotfiles/zsh/.zim/modules/zim-yq/init.zsh
-source /dotfiles/zsh/.zim/modules/zim-gopass/init.zsh
+zimfw() { source "$HOME/.zim/zimfw.zsh" "${@}" }
+zmodule() { source "$HOME/.zim/zimfw.zsh" "${@}" }
+fpath=("$HOME/.zim/modules/git/functions" "$HOME/.zim/modules/zsh-completions/src" "$HOME/.zim/modules/zsh-completions/src" "$HOME/.zim/modules/zsh-completions/src" ${fpath})
+autoload -Uz -- git-alias-lookup git-branch-current git-branch-delete-interactive git-branch-remote-tracking git-dir git-ignore-add git-root git-stash-clear-interactive git-stash-recover git-submodule-move git-submodule-remove
+source "$HOME/.zim/modules/git/init.zsh"
