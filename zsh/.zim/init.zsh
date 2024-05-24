5d2c5adb344124cad38b1edeaac34f42a6010882
mfaine <{'changed': False, 'results': [], 'failed': False}>
2024-05-24 14:46:19 -0500

    Update dotfiles and paths

--- zsh/.zim/init.zsh
+++
@@ -1,5 +0,0 @@
-zimfw() { source "$HOME/.zim/zimfw.zsh" "${@}" }
-zmodule() { source "$HOME/.zim/zimfw.zsh" "${@}" }
-fpath=("$HOME/.zim/modules/git/functions" "$HOME/.zim/modules/zsh-completions/src" "$HOME/.zim/modules/zsh-completions/src" "$HOME/.zim/modules/zsh-completions/src" ${fpath})
-autoload -Uz -- git-alias-lookup git-branch-current git-branch-delete-interactive git-branch-remote-tracking git-dir git-ignore-add git-root git-stash-clear-interactive git-stash-recover git-submodule-move git-submodule-remove
-source "$HOME/.zim/modules/git/init.zsh"
