bc73704f1b84f3937b05aca2d0a12f76998b5d5c
mfaine <{'changed': False, 'results': [], 'failed': False}>
2024-06-03 16:01:35 -0500

    Adds toggles for visual markers

--- nvim/nvim/lua/chadrc.lua
+++ nvim/nvim/lua/chadrc.lua
@@ -2,6 +2,20 @@
 -- https://github.com/NvChad/NvChad/blob/v2.5/lua/nvconfig.lua
 
 ---@type ChadrcConfig
+local utils = require "nvchad.stl.utils"
 local M = {}
 M.ui = {theme = "catppuccin"}
+M.cwd = function()
+  local name = vim.loop.cwd()
+  name = "%#St_cwd# 󰉖 " .. (name:match "([^/\\]+)[/\\]*$" or name) .. " "
+  return (vim.o.columns > 85 and name) or ""
+end
+M.ui.statusline = {
+    theme = "vscode_colored", -- default/vscode/vscode_colored/minimal
+    -- default/round/block/arrow separators work only for default statusline theme
+    -- round and block will work for minimal theme only
+    separator_style = "round",
+    order = { "mode", "file", "git", "%=", "lsp_msg", "%=", "diagnostics", "lsp", "cursor", "cwd" }, 
+    modules = nil,
+  }
 return M
--- nvim/nvim/lua/mappings.lua
+++ nvim/nvim/lua/mappings.lua
@@ -3,8 +3,21 @@ require "nvchad.mappings"
 -- add yours here
 
 local map = vim.keymap.set
-
+-- local wk = require("which-key")
 map("n", ";", ":", { desc = "CMD enter command mode" })
 map("i", "jk", "<ESC>")
 
--- map({ "n", "i", "v" }, "<C-s>", "<cmd> w <cr>")
+-- Mapping to toggle indentation lines
+map("n", "<leader>ti", function() require("ibl").setup_buffer(0, { enabled = not require("ibl.config").get_config(0).enabled, }) end, { desc = "Toggle Indention Lines"})
+
+-- Mapping to toggle Gitsigns
+map("n", "<leader>tg", "<cmd>Gitsigns toggle_signs<cr>" , { desc = "Toggle Indention Lines"})
+
+-- Mapping to toggle all visible markings
+map("n", "<leader>ta", function() 
+  require("ibl").setup_buffer(0, { enabled = not require("ibl.config").get_config(0).enabled, })
+  vim.cmd(string.format("%s", "set nu!"))
+  vim.cmd(string.format("%s", "Gitsigns toggle_signs"))
+end, { desc = "Toggle All Visible Markers"})
+
+require("ibl").setup_buffer(0, { enabled = true, })
