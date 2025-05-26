c6ab2c8bb0398dbdef1316e958704c42b81067bc
Mark Faine <mark.faine@nasa.gov>
2025-05-26 15:35:56 -0500

    Remove obsolete files, add comments for TODOs

--- Hooks/zsh/post.sh
+++ Hooks/zsh/post.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
 pushd "$HOME" &>/dev/null || exit 1
+# This can't work until after credentials exist
+# TODO: figure out another way
 # doppler secrets download --no-file --format env > .env
 popd &>/dev/null
